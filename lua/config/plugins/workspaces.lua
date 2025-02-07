15097738130db5992be961181c16f605ecdd64dc
Ernesto Rodriguez Reina <erreina@gmail.com>
2025-02-07 13:52:52 -0500

    Fixed keybinding for telescope workspaces

--- lua/config/plugins/workspaces.lua
+++ lua/config/plugins/workspaces.lua
@@ -1,20 +1,19 @@
 return {
-  'natecraddock/workspaces.nvim',
-  dependencies = {
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
-  },
-  keys = {
-    {
-      "<leader>fw",
-      "<cmd>Telescope workspaces<cr>",
-      desc = "Workspaces",
-    },
-  },
-  config = function()
-    require("workspaces").setup({
-    })
-  end,
+	"natecraddock/workspaces.nvim",
+	dependencies = {
+		{
+			"nvim-telescope/telescope.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+		},
+	},
+	keys = {
+		{
+			"<leader>fW",
+			"<cmd>Telescope workspaces<cr>",
+			desc = "Workspaces",
+		},
+	},
+	config = function()
+		require("workspaces").setup({})
+	end,
 }
